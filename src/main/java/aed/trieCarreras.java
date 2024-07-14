55fab9349c381f18615b61dc541e80f7376b9106
FGRiess <riessfranco1@gmail.com>
2024-07-13 22:32:56 -0300

    Update trieCarreras.java
    Complejidad inscriptosEnMateria

--- src/main/java/aed/trieCarreras.java
+++ src/main/java/aed/trieCarreras.java
@@ -198,35 +198,35 @@ public class trieCarreras {
         }
     }
 
-    public void insertarAlumno(String carrera, String materia, String alumno){    //Complejidad de la función: O
+    public void insertarAlumno(String carrera, String materia, String alumno){    //Complejidad de la función: O(|c|+|m|)
         if(perteneceCarrera(carrera)){                                                //O(|c|)
             Nodo actual = raiz;                                                       //O(1)
-            for(int i=0;i<carrera.length();i++){                                      //Ciclo: //Guarda: 3*O(1) = O(1)
-                while(actual.valor != carrera.charAt(i)){                                 //Ciclo: //Guarda: O(1)
+            for(int i=0;i<carrera.length();i++){                                      //Ciclo: O(|c|) //Guarda: 3*O(1) = O(1)
+                while(actual.valor != carrera.charAt(i)){                                 //Ciclo: O(1) //Guarda: O(1)
                     actual = actual.hermano;                                                  //O(1)
                 }
                 if(i<(carrera.length()-1)){                                               //If completo: 2*O(1) = O(1) //Guarda: O(1)
                     actual = actual.hijo;                                                     //O(1)
                 }
             }
-            actual.materias.insertarAlumno(materia, alumno);                          //?????????????????????????????????????????????????????
+            actual.materias.insertarAlumno(materia, alumno);                         //o(|m|)
         }
     }
 
-    public int inscriptosEnMateria(String carrera, String materia){
-        if(perteneceCarrera(carrera)){
-            Nodo actual = raiz;
-            for(int i=0;i<carrera.length();i++){
-                while(actual.valor != carrera.charAt(i)){
-                    actual = actual.hermano;
+    public int inscriptosEnMateria(String carrera, String materia){       //Complejidad de la función: O(|c|+|m|)
+        if(perteneceCarrera(carrera)){    //Peor caso        //O(|c|) 
+            Nodo actual = raiz;    //O(1)
+            for(int i=0;i<carrera.length();i++){        //Ciclo: O(|c|)  //Guarda: O(1)
+                while(actual.valor != carrera.charAt(i)){    //Ciclo: O(1)  //Guarda: O(1)
+                    actual = actual.hermano;    //O(1)
                 }
-                if(i<(carrera.length()-1)){
-                    actual = actual.hijo;
+                if(i<(carrera.length()-1)){    //O(1)
+                    actual = actual.hijo;    //O(1)
                 }
             }
-            return actual.materias.inscriptos(materia);
-        }else{
-            return 0;
+            return actual.materias.inscriptos(materia);    //O(|m|)
+        }else{    //Mejor caso
+            return 0;    //O(1)
         }
     }
 
